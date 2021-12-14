df131318cd869c8ab1560eabef13a9fe819944f9
Diana Koroma <dianakirstinek@hotmail.com>
2021-12-14 14:38:04 +0100

    forside færdig med tilsluttet javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+/* javascript taget fra Youtube til burger menu – https://www.youtube.com/watch?v=oYRda7UtuhA&t=1337s&ab_channel=EasyTutorials */
+
+
+var navLinks = document.getElementById("navLinks");
+
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
