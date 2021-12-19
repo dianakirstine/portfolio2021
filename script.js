40cf8860bad4e2f53685626a2cd793b3dc6b52c7
Diana Koroma <dianakirstinek@hotmail.com>
2021-12-19 01:45:31 +0100

    endelige rettelser efter validering

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /* javascript taget fra Youtube til burger menu – https://www.youtube.com/watch?v=oYRda7UtuhA&t=1337s&ab_channel=EasyTutorials */
 
 
+/* skifter mellem at vise / skjule navigationens links når brugeren klikker på 
+burgerens ikoner */
+
 var navLinks = document.getElementById("navLinks");
 
 
